48196228d99d3da425288db46e30c960d43cc2ce
Ryan Ross <ryan@reross.com>
2022-03-08 18:12:09 -0600

    adding finished project to git

---
+++ src/comps/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Postigram</h1>
+      <h2>Photo Gallery</h2>
+    </div>
+  );
+};
+
+export default Title;
